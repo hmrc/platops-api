c76cfbb0d0983dda04f39c09b49df1d782388bf1
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2023-10-12 13:55:29 +0100

    pr-commenter suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.14.0"
+  val bootstrapVersion = "7.22.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
